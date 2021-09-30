761433c809a97f2e554c66e90f7b2fc2880debf3
DELL <adamwawrzyniak5@gmail.com>
2021-09-30 22:12:01 +0200

    Dodanie obliczania obwodu prostokąta.

--- Zadanie4.py
+++ Zadanie4.py
@@ -24,6 +24,29 @@ def circle_field():
     field = math.pi*radius**2
     return field
 
+def side_a():
+    '''Obliczanie długości boku A'''
+    if x1 > x2:
+        side_a = x1-x2
+    else:
+        side_a = x2-x1
+    return side_a
+
+def side_b():
+    if y2 > y1:
+        side_b = y2 - y1
+    else:
+        side_b = y1 - y2
+    return side_b
+
+def parimeter():
+    parimeter = 2*side_b()+2*side_a()
+    return parimeter
+
+def rectangle_area():
+    area = 1
+
+
 
 x1 = int(input("Podaj współrzędną x, punktu A :"))
 y1 = int(input("Podaj współrzędną y, punktu A :"))
@@ -36,4 +59,5 @@ print(f"Długość odcinka AB wynosi: {round(section_lenght(),2)}")
 print(f"Pole powierzchni koła o średnicy długości odćnika AB wynosi:"
       f" {round(circle_field(),2)} jednostek kwadratowych")
 print(f"Promień okręgu wynosi {round(section_lenght()/2,2)}")
+print(f"Obwód prostokąta {parimeter()}")
 
